601c8561d14c09f4cfcfcc5a6f3c83c8e30b3a6b
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-05-13 11:18:46 +0300

    Added class for tiles.

---
+++ Enums/TileTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace HexWorld.Enums
+{
+    public enum TileTypes
+    {
+    Ocean,
+    Desert,
+    Jungle,
+    Forest,
+    Mountain,
+    Grassland,
+    Swamp,
+    Steppe,
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using HexWorld.Enums;
+
+namespace HexWorld
+{
+    public class Tile
+    {
+        public TileTypes Type { get; set; }
+        public bool IsWater { get; }
+
+        public Tile(TileTypes type)
+        {
+            Type = type;
+            switch (Type)
+            {
+                case TileTypes.Ocean:
+                    IsWater = true;
+                    break;
+                case TileTypes.Desert:
+                    IsWater = false;
+                    break;
+                default:
+                    throw new ArgumentException("Unknown Tile Type");
+            }
+        }
+    }
+}
