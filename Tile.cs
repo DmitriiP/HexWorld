608f255900c693e2791b1062c08ed347dd920ed4
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-05-14 19:18:59 +0300

    Converted Tile.cs to \n line endings. Added Printable property and ability to change tile from outside.

--- Tile.cs
+++ Tile.cs
@@ -6,22 +6,35 @@ namespace HexWorld
     public class Tile
     {
         public TileTypes Type { get; set; }
-        public bool IsWater { get; }
+        public bool IsWater { get; set; }
+        public string Printable { get; set; }
 
         public Tile(TileTypes type)
+        {
+            ChangeTile(type);
+        }
+
+        public void ChangeTile(TileTypes type)
         {
             Type = type;
             switch (Type)
             {
                 case TileTypes.Ocean:
                     IsWater = true;
+                    Printable = "▓▓▓";
                     break;
                 case TileTypes.Desert:
                     IsWater = false;
+                    Printable = "░░░";
                     break;
                 default:
                     throw new ArgumentException("Unknown Tile Type");
             }
+        }
+
+        public override string ToString()
+        {
+            return Printable;
         }
     }
 }
